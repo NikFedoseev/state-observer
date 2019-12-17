96b1b76ada671b2f370110ae3e9bbed12e4f5b45
Nikita Fedoseev <n.fedoseev@onpoint.ru>
2019-12-17 16:17:59 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,41 +1,39 @@
 import isEqual from "lodash.isequal";
-(window => {
-  const createStore = defaultState => {
-    let state = defaultState || {};
-    let listeners = [];
+const createStore = defaultState => {
+  let state = defaultState || {};
+  let listeners = [];
 
-    function getState() {
-      return state;
-    }
-
-    function setState(newState) {
-      const prevState = getState();
-      state = { ...prevState, ...newState };
-      listeners.forEach(listener => listener(getState(), prevState));
-    }
-
-    function connect(mapStateToProps, func) {
-      const connectedFunc = (globalState, prevGlobalState) => {
-        const localState = mapStateToProps(globalState);
-        const prevLocalState = mapStateToProps(prevGlobalState);
-        const isStatesEqual = isEqual(localState, prevLocalState);
-        if (isStatesEqual) return;
-        func(localState);
-      };
-      listeners.push(connectedFunc);
-      return connectedFunc;
-    }
-
-    return {
-      getState,
-      setState,
-      connect
-    };
-  };
-
-  if (typeof module != "undefined" && module.exports) {
-    module.exports = createStore;
-  } else {
-    window.createStore = createStore;
+  function getState() {
+    return state;
   }
-})(window);
+
+  function setState(newState) {
+    const prevState = getState();
+    state = { ...prevState, ...newState };
+    listeners.forEach(listener => listener(getState(), prevState));
+  }
+
+  function connect(mapStateToProps, func) {
+    const connectedFunc = (globalState, prevGlobalState) => {
+      const localState = mapStateToProps(globalState);
+      const prevLocalState = mapStateToProps(prevGlobalState);
+      const isStatesEqual = isEqual(localState, prevLocalState);
+      if (isStatesEqual) return;
+      func(localState);
+    };
+    listeners.push(connectedFunc);
+    return connectedFunc;
+  }
+
+  return {
+    getState,
+    setState,
+    connect
+  };
+};
+
+if (typeof module != "undefined" && module.exports) {
+  module.exports = createStore;
+} else {
+  window.createStore = createStore;
+}
